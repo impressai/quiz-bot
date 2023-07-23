435e01aa03d6f6cca7063527fb122c5971126ffe
Haris M E <harisme93@gmail.com>
2023-07-23 23:46:02 +0530

    Initial working version

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quiz bot
+
+Steps to run the project
+
+1. Install Docker and Docker Compose (https://docs.docker.com/compose/install/)
+2. Docker should be running
+3. In the project root run `docker-compose up`
+4. Go to `localhost` to view the chatbot
---
+++ core/reply_factory.py
@@ -0,0 +1,52 @@
+
+from .constants import BOT_WELCOME_MESSAGE, PYTHON_QUESTION_LIST
+
+
+def generate_bot_responses(message, session):
+    bot_responses = []
+
+    current_question_id = session.get("current_question_id")
+    if not current_question_id:
+        bot_responses.append(BOT_WELCOME_MESSAGE)
+
+    success, error = record_current_answer(message, current_question_id)
+
+    if not success:
+        return [error]
+
+    next_question, next_question_id = get_next_question(current_question_id)
+
+    if next_question:
+        bot_responses.append(next_question)
+    else:
+        final_response = generate_final_response(session)
+        bot_responses.append(final_response)
+
+    session["current_question_id"] = next_question_id
+    session.save()
+
+    return bot_responses
+
+
+def record_current_answer(answer, current_question_id):
+    '''
+    Validates and stores the answer for the current question to session.
+    '''
+    return True, ""
+
+
+def get_next_question(current_question_id):
+    '''
+    Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
+    '''
+
+    return "dummy question", -1
+
+
+def generate_final_response(session):
+    '''
+    Creates a final result message including a score based on the answers
+    by the user for questions in the PYTHON_QUESTION_LIST.
+    '''
+
+    return "dummy result"
